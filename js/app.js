f2242937c21c4dae2b9b9ca82058564ef3b93bda
Cédryc Ruth <ceruth@epfc.eu>
2021-04-29 17:41:26 +0200

    Get all wines - premier test

--- js/app.js
+++ js/app.js
@@ -3,3 +3,51 @@
  *
  */
  
+//Avec l'API Fetch (cours de la semaine prochaine)
+/*
+fetch('https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines')
+	  .then(response => response.json())
+	  .then(json => console.log(json));
+	  
+*/
+
+//Avec XMLHttpRequest
+const xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+	if(this.status==200) {
+		const response = this.responseText;
+		const data = JSON.parse(response);
+		
+		console.log(data);
+		
+		//Afficher le nom des vins dans la page
+		let strLis = '';
+		
+		for(let i in data) {
+			strLis += `<li>${data[i].name}</li>`;
+		}
+		
+		//jQuery
+		//$('#liste').html(strLis);
+		
+		//Vanilla JS
+		const listeUL = document.querySelector('#liste');
+		
+		for(let i in data) {
+			let li = document.createElement('li');
+			li.innerText = data[i].name;
+			
+			li.addEventListener('click', function(e) {
+				this.style.backgroundColor = 'green';
+			});
+			
+			listeUL.appendChild(li);
+		}
+	}
+}
+
+xhr.open('GET','https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines',true);
+xhr.send();
+
+
